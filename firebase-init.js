ab5b2d1f749479b1514e91ba6cfa98252f227281
Suriya0809 <164842049+Suriya0809@users.noreply.github.com>
2025-06-02 19:25:39 +0530

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,13 @@
+// js/firebase-init.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDXTPYTrDM3ZloJrBGaoMOANClPEYNWTZQ",
+  authDomain: "employee-management-syst-990f5.firebaseapp.com",
+  databaseURL: "https://employee-management-syst-990f5-default-rtdb.firebaseio.com",
+  projectId: "employee-management-syst-990f5",
+  storageBucket: "employee-management-syst-990f5.appspot.com",
+  messagingSenderId: "302985035923",
+  appId: "1:302985035923:web:8848684a54c5739b9b8d66",
+  measurementId: "G-QRKEC2WXN8"
+};
+
+firebase.initializeApp(firebaseConfig);
